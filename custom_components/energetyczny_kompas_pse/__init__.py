73c8d8debd71966e09fbf120227172c88993a9b4
Krzysztof Lewandowski <krzysiek@lca.pl>
2025-01-07 14:15:28 +0100

    1.1

--- custom_components/energetyczny_kompas_pse/__init__.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "energetyczny_kompas_pse"
-
-async def async_setup(hass, config):
-    """Set up Energetyczny Kompas PSE component."""
-    return True
-
-async def async_setup_entry(hass, entry):
-    """Set up Energetyczny Kompas PSE from a config entry."""
-    return True
--- custom_components/energetyczny_kompas_pse/const.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "energetyczny_kompas_pse"
-
-SENSOR_NAME = "Energetyczny Kompas"
-STATES = [
-    "ZALECANE UŻYTKOWANIE",
-    "NORMALNE UŻYTKOWANIE",
-    "ZALECANE OSZCZĘDZANIE",
-    "WYMAGANE OGRANICZANIE",
-]
