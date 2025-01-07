703b7ea16f365cd6e6829f37d832d5de0c520fa5
Krzysztof Lewandowski <krzysiek@lca.pl>
2025-01-07 18:56:50 +0100

    1.6

--- custom_components/energetyczny_kompas_pse/__init__.py
+++ custom_components/energetyczny_kompas_pse/__init__.py
@@ -10,10 +10,13 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Energetyczny Kompas PSE from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    # Forward the setup to the sensor platform
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
