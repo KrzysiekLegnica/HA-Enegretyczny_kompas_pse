3f4842c8df1792750dc908e0b7b2b22b29650e0b
Krzysztof Lewandowski <60132935+KrzysiekLegnica@users.noreply.github.com>
2025-01-07 16:48:08 +0100

    Update __init__.py

---
+++ custom_components/energetyczny_kompas_pse/__init__.py
@@ -0,0 +1,18 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up Energetyczny Kompas PSE component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Energetyczny Kompas PSE from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
